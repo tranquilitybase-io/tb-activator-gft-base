351547d4d22cd0f0fd64ad03eaa9182a24bce5b7
Arvind Clement <33026513+arvindclement@users.noreply.github.com>
2020-05-16 14:08:39 +0100

    Update connections.tf

--- connections.tf
+++ connections.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  credentials = file("/opt/app/data/service-account.json")
-  #credentials = file("../service-account.json")
+  #credentials = file("/opt/app/data/service-account.json")
+  credentials = file("../service-account.json")
   project     = var.host_project_id
   region      = var.region
 }
